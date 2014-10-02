a4a1bd791aa69a98e4eb1506c67472a7a7487fdc
Gustavo <gugahoa@hotmail.com>
2014-10-01 23:24:50 -0300

    First commit.

---
+++ tibiaclient.py
@@ -0,0 +1,64 @@
+import os
+import ptrace
+import binascii
+import versions
+
+class TibiaClient:
+	def __init__(self, version):
+		self.version = version
+		self.pid = 0
+
+	def searchTibia(self):
+		process_list = os.popen("pidof -s Tibia")
+		pid = process_list.readlines()
+
+		if (pid):
+			self.pid = int(pid[0])
+			print "Tibia process found at " + str(self.pid)
+
+			memory_map = open('/proc/' + str(self.pid) + '/maps', 'r')
+			memory_map.readline() ##Skip one line.
+
+			base_addr = memory_map.readline()[:8]
+			self.base_addr = int(base_addr, 16)
+
+			print "Base addr is: " + str(hex(self.base_addr))
+
+			return self.pid
+
+		return 0
+
+	def attach(self):
+		if (self.pid == 0):
+			print "Unable to attach to Tibia process."
+			return 0
+
+		ptrace.attach(self.pid)
+		print "Succesfully attached to Tibia process."
+
+		return self.pid
+
+	def detach(self):
+		if (self.pid == 0):
+			print "Unable to detach to Tibia process."
+			return 0
+
+		ptrace.detach(self.pid, 18)
+		print "Succesfully detached to Tibia process."
+
+		self.pid = 0
+		return 1
+
+	def writeToMemory(self, addr, value):
+		if (self.pid == 0):
+			print "Unable to write to process memory."
+			return 0
+
+		ptrace.pokedata(self.pid, self.base_addr + addr, value)
+
+	def readFromMemory(self, addr):
+		if (self.pid == 0):
+			print "Unable to read from process memory."
+			return 0
+
+		return ptrace.peekdata(self.pid, self.base_addr + addr)
---
+++ versions.py
@@ -0,0 +1,8 @@
+Versions = {
+	'8.60': {
+			'ip_addr' : 0x1638E0,
+			'ver_addr' : 0x921B,
+			'port_addr' : 0x163944,
+			'dist' : 112
+			}
+	}
