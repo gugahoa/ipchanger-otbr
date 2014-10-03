3bf44f30e32dcc33cde1e8e312516a5414df9e7c
Gustavo <gugahoa@hotmail.com>
2014-10-03 05:37:21 -0300

    Now insert the right ip addres

--- main.py
+++ main.py
@@ -7,16 +7,19 @@ tibiaclient = tibiaclient.TibiaClient('8.60');
 tibiaclient.searchTibia();
 
 if tibiaclient.attach() == 0:
-	exit()
+	quit()
 
-ip = "127.0.0.1"
+ip = "baiaklast.no-ip.biz"
+print "ip: " + ip
+ip = ip[::-1]
+
 ip = binascii.hexlify(ip)
 
 ips = []
 
 for offset, _ in enumerate(ip):
-	if ((offset + 8) % 8 == 0):
-		ips.insert(offset/8, ip[offset:offset+7])
+	if ((offset + 16) % 16 == 0):
+		ips.insert(offset/16, ip[offset:offset+16])
 
 port = 7171
 
@@ -30,22 +33,18 @@ data = data[:4]
 if data == tibiaclient.version:
 	print "Correct Tibia version."
 else:
-	exit()
+	quit()
 
 for x in range(10):
-	for ips_ in ips:
-		tibiaclient.writeToMemory(versions.Versions[tibiaclient.version]['ip_addr'] + x*versions.Versions[tibiaclient.version]['dist'], int(ips_, 16))
+	offset = 0
+	for ips_ in reversed(ips):
+		tibiaclient.writeToMemory(versions.Versions[tibiaclient.version]['ip_addr'] + x*versions.Versions[tibiaclient.version]['dist'] + offset, int(ips_, 16))
+		offset = offset + len(ips_)/2
+		print binascii.unhexlify(ips_)[::-1]
+
 	tibiaclient.writeToMemory(versions.Versions[tibiaclient.version]['port_addr'] + x*versions.Versions[tibiaclient.version]['dist'], port)
 
-data = tibiaclient.readFromMemory(versions.Versions[tibiaclient.version]['ip_addr'])
+
 tibiaclient.detach();
 
-data = hex(data)
-
-print "Done\n\n"
-print data
-
-data = binascii.unhexlify(data[2:])
-
-data = ''.join(reversed(data))
-print data
+print "Done"
