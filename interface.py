54652ab87086986cdfd86996398348799c741c5a
Gustavo Aguiar <gugahoa@gmail.com>
2015-04-21 06:29:56 -0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -30,13 +30,13 @@ class Interface(Gtk.Window):
 		hbox.pack_start(self.entry, True, True, 0)
 
 		hbox = Gtk.Box(orientation = Gtk.Orientation.HORIZONTAL)
-		self.model = Gtk.ListStore(int, int)
+		self.model = Gtk.ListStore(int)
 		self.client_list = Gtk.ComboBox.new_with_model(self.model)
 		self.client_list.connect("changed", self.selectClient)
 
 		renderer_text = Gtk.CellRendererText()
 		self.client_list.pack_start(renderer_text, True)
-		self.client_list.add_attribute(renderer_text, "text", 1)
+		self.client_list.add_attribute(renderer_text, "text", 0)
 
 		self.button = Gtk.Button.new_from_icon_name(icon_name = "gtk-refresh", size = 2)
 		self.button.set_tooltip_text("Update client list")
@@ -55,7 +55,7 @@ class Interface(Gtk.Window):
 		self.connect("delete-event", self.closeWindow)
 
 		self.tibia_proc = {}
-		self.list = {}
+		self.open_clients = {}
 		self.selected_version = 0
 
 		self.updateClients(None)
@@ -63,46 +63,47 @@ class Interface(Gtk.Window):
 	def selectClient(self, combo):
 		tree_iter = combo.get_active_iter()
 		if tree_iter != None:
-			model = combo.get_model()
-			self.selected_version = model[tree_iter][1]
+			# Should I send an error window instead?
+			if self.model[tree_iter][0] in self.open_clients and self.open_clients[self.model[tree_iter][0]] == []:
+				print("Tibia %s not found." % self.model[tree_iter][0])
+				self.model.remove(tree_iter)
+			else:
+				self.selected_version = self.model[tree_iter][0]
 
 	def updateClients(self, widget):
 		self.pids = utils.find_pid_by_name("Tibia")
-		if len(self.pids) > 0:
+		if len(self.pids) >= 0:
 			for tpid in self.pids:
 				if tpid not in self.tibia_proc:
 					self.tibia_proc[tpid] = TibiaProcess(tpid)
+
 				version = self.tibia_proc[tpid].getVersion()
+				if version not in self.open_clients:
+					self.model.append([version])
+					self.open_clients[version] = []
 
-				if version not in self.list:
-					self.model.append([0, version])
-					self.list[version] = []
+				if tpid not in self.open_clients[version]:
+					self.open_clients[version].append(tpid)
 
-				if tpid not in self.list[version]:
-					self.list[version].append(tpid)
-			for version, tpids in self.list.items():
+			for version, tpids in self.open_clients.items():
 				for tpid in tpids:
 					if tpid not in self.pids:
 						tpids.remove(tpid)
+						if tpid in self.tibia_proc:
+							del self.tibia_proc[tpid]
 		else:
-			self.list = {}
+			self.open_clients = {}
 			self.tibia_proc = {}
 			self.selected_version = 0
 
 	def changeIp(self, widget):
 		self.updateClients(None)
-		if self.selected_version == 0 or self.list[self.selected_version] == []:
-			print("No Tibia of such version open (%s)" % self.selected_version)
+		# Should I send an error window instead?
+		if self.selected_version == 0 or self.open_clients[self.selected_version] == []:
+			print("Tibia %s not found." % self.selected_version)
 			return
 
-		for tpid in self.list[self.selected_version]:
-			if tpid not in self.pids:
-				if tpid in self.tibia_proc:
-					del self.tibia_proc[tpid]
-				continue
-			if tpid not in self.tibia_proc:
-				self.tibia_proc[tpid] = TibiaProcess(tpid)
-
+		for tpid in self.open_clients[self.selected_version]:
 			self.tibia_proc[tpid].attach()
 			self.tibia_proc[tpid].changeIp(self.entry.get_text())
 			self.tibia_proc[tpid].changeRsa()
